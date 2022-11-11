e42d8bae2c222800104fb7245598aa943c90c2cb
Mintaek_CHO <mintaek5555@gmail.com>
2022-11-11 23:17:27 +0900

    헤로쿠 배포 테스트

--- src/test/java/com/project/sns/SnsApplicationTests.java
+++ src/test/java/com/project/sns/SnsApplicationTests.java
@@ -3,7 +3,7 @@ package com.project.sns;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest //시작
 class SnsApplicationTests {
 
 	@Test
